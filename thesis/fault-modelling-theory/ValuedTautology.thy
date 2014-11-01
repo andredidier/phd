5aefac4b170d8010d2c20512d67a2e4eac3cc6e2
Andre Didier <alrd+phd@cin.ufpe.br>
2014-10-31 21:15:49 -0300

    Implementação com conjunto indexado.

--- thesis/fault-modelling-theory/ValuedTautology.thy
+++ thesis/fault-modelling-theory/ValuedTautology.thy
@@ -33,42 +33,50 @@ definition ValuedBool_bool_eval :: "('vb,'vv) ValuedBool \<Rightarrow> ('vb \<Ri
 definition ValuedBool_value_eval :: "('vb, 'vv) ValuedBool \<Rightarrow> ('vv \<Rightarrow> Values) \<Rightarrow> Values" where 
   "ValuedBool_value_eval VB valuation \<equiv> ValuesOperand_eval (snd VB) valuation"
 
-type_synonym ('vb, 'vv) ValuedBoolExp = "(('vb,'vv) ValuedBool) list"
+type_synonym ('vb, 'vv) ValuedBoolExp = "(nat \<times> (('vb,'vv) ValuedBool)) set"
 
-primrec ValuedBoolExp_bool_eval :: "('vb,'vv) ValuedBoolExp \<Rightarrow> ('vb \<Rightarrow> bool) \<Rightarrow> bool" where
-  "ValuedBoolExp_bool_eval [] val_b = False" |
-  "ValuedBoolExp_bool_eval (s # ss) val_b = 
-    ((ValuedBool_bool_eval s val_b) \<or> (ValuedBoolExp_bool_eval ss val_b))"
+definition ValuedBoolExp_bool_eval :: "('vb,'vv) ValuedBoolExp \<Rightarrow> ('vb \<Rightarrow> bool) \<Rightarrow> bool" where
+  "ValuedBoolExp_bool_eval Es val_b \<equiv> (\<exists> E \<in> Es. ValuedBool_bool_eval (snd E) val_b)"
 
+lemma to_be_or_not_to_be1 : "(BoolOperand_eval E val_b) \<or> (BoolOperand_eval (VBBExpUnOp Not E) val_b)"
+apply (auto)
+done
+
+lemma to_be_or_not_to_be2 : "ValuedBoolExp_bool_eval { (1,(E,V)), (2,(VBBExpUnOp Not E, U))  } val_b"
+apply (auto simp add: to_be_or_not_to_be1 ValuedBoolExp_bool_eval_def ValuedBool_bool_eval_def )
+done
+
+lemma to_be_or_not_to_be3 : 
+  "(\<not> (ValuedBool_bool_eval (VBBExpUnOp Not E,V) val_b)) \<Longrightarrow> (ValuedBool_bool_eval (E,U) val_b)"
+apply (auto simp add: ValuedBool_bool_eval_def)
+done
+
+definition IndexedValuedBoolExp :: "('vb, 'vv) ValuedBoolExp \<Rightarrow> bool" where
+  "IndexedValuedBoolExp Es \<equiv> (\<forall> i j VBi VBj . ((i, VBi) \<in> Es \<and> (j, VBj) \<in> Es \<and> (i = j)) \<longrightarrow> (VBi = VBj))"
 
 definition ValuedTautology :: "('vb, 'vv) ValuedBoolExp \<Rightarrow> ('vb \<Rightarrow> bool) \<Rightarrow> ('vv \<Rightarrow> Values) \<Rightarrow> bool" where
-  "ValuedTautology VBExp val_b val_v \<equiv> (
-      (ValuedBoolExp_bool_eval VBExp val_b) \<and>
-      (ValuedTautology_set (ValuedBoolExp_to_set VBExp) val_b val_v)
+  "ValuedTautology Es val_b val_v \<equiv> (
+      (IndexedValuedBoolExp Es) \<and>
+      (ValuedBoolExp_bool_eval Es val_b) \<and>
+      (\<forall> i j VBi VBj . (i \<noteq> j) \<and> ((i,VBi) \<in> Es) \<and> ((j,VBj) \<in> Es) \<and> 
+        (((ValuedBool_bool_eval VBi val_b) \<and> (ValuedBool_bool_eval VBj val_b)) \<longrightarrow>  
+          ((ValuedBool_value_eval VBi val_v) = (ValuedBool_value_eval VBj val_v))) )
     )"
 
-lemma not_valued_tautology1 : "\<not> ValuedTautology (SVBExp (VBBVarOp A, U)) val_b val_v"
-apply (auto simp add: ValuedTautology_def ValuedTautology_set_def)
+lemma not_valued_tautology1 : "\<not> ValuedTautology {(1,(VBBVarOp A, U))} val_b val_v"
+apply (auto simp add: ValuedTautology_def)
 done
 
 lemma not_valued_tautology2 : 
-  "\<not> ValuedTautology (BVBExp (op \<or>) (SVBExp (VBBVarOp A, U)) (SVBExp (VBBVarOp B, V))) val_b val_v"
-apply (auto simp add: ValuedTautology_def ValuedTautology_set_def)
+  "\<not> ValuedTautology {(1,(VBBVarOp A, U)), (2,(VBBVarOp B, V)) } val_b val_v"
+apply (auto simp add: ValuedTautology_def)
 done
 
-lemma valued_tautology_or : 
-  "(BoolOperand_eval A val_b) \<Longrightarrow> (BoolOperand_eval B val_b) \<Longrightarrow>
-  (((BoolOperand_eval A val_b) \<and> (BoolOperand_eval B val_b)) \<longrightarrow> (U = V)) \<Longrightarrow> 
-  ValuedTautology 
-    (BVBExp (op \<or>)
-      (BVBExp (op \<or>) 
-        (SVBExp (A, U))
-        (SVBExp (B, V)))
-      (SVBExp ((VBBExpBinOp (op \<and>) 
-                (VBBExpUnOp Not A) 
-                (VBBExpUnOp Not B)), Q))) 
-    val_b val_v"
-apply (auto simp add: ValuedTautology_def ValuedTautology_set_def ValuedBool_bool_eval_def)
+lemma valued_tautology_basic_or : "(ValuedTautology { (1,(E,V)), (2,(VBBExpUnOp Not E,U)) } val_b val_v)"
+apply (simp add: ValuedTautology_def IndexedValuedBoolExp_def ValuedBoolExp_bool_eval_def to_be_or_not_to_be3
+  ValuedBool_bool_eval_def ValuedBool_value_eval_def BoolOperand_eval_def ValuesOperand_eval_def)
+
 done
+
 
 end
