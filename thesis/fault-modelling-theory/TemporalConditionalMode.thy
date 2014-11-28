f6a647af16e9cb750b2f1039ff6e80e2436cabc5
Andre Didier <alrd+phd@cin.ufpe.br>
2014-11-28 16:33:00 -0300

    Falta resolver a variável.

--- thesis/fault-modelling-theory/TemporalConditionalMode.thy
+++ thesis/fault-modelling-theory/TemporalConditionalMode.thy
@@ -1,57 +1,8 @@
 theory TemporalConditionalMode
 
-imports ConditionalMode
+imports Main
 
 begin
-
-datatype_new 'a temporal_exp = 
-  Top_temporal_exp
-  | Bot_temporal_exp
-  | Atom_temporal_exp 'a
-  | Before_temporal_exp "'a temporal_exp" "'a temporal_exp"
-  | Or_temporal_exp "'a temporal_exp" "'a temporal_exp"
-
-abbreviation And_temporal_exp :: "'a temporal_exp binop"
-where
-  "And_temporal_exp t1 t2 \<equiv> 
-    Or_temporal_exp 
-      (Before_temporal_exp t1 t2) 
-      (Before_temporal_exp t2 t1)"
-
-fun is_ordered_sublist :: "'a list \<Rightarrow> 'a list \<Rightarrow> bool"
-where
-  "is_ordered_sublist (a1 # ls1) (a2 # ls2) = ((a1 = a2) \<and> is_ordered_sublist ls1 ls2)" |
-  "is_ordered_sublist [] _ = True" |
-  "is_ordered_sublist _ [] = False"
-
-definition unpile_temporal_exp :: "'a list \<Rightarrow> 'a list set \<Rightarrow> 'a list set"
-where
-  "unpile_temporal_exp ls A \<equiv> { t . t \<in> A \<and> is_ordered_sublist ls t} "
-
-inductive_set
-  ta :: "'a list set set set"
-where
-  var: "{L | L ls . ls \<in> L \<and> a \<in> set ls \<and> distinct ls} \<in> ta"
-| Compl: "S \<in> ta \<Longrightarrow> - S \<in> ta"
-| inter: "S \<in> ta \<Longrightarrow> T \<in> ta \<Longrightarrow> S \<inter> T \<in> ta"
-
-lemma ta_empty: "({} :: 'a list set set) \<in> ta"
-proof -
-  obtain S :: "'a list set set" where "S \<in> ta" by (fast intro: ta.var)
-  hence "S \<inter> - S \<in> ta" by (intro ta.intros)
-  thus ?thesis by simp
-qed
-
-typedef 'a temporal_formula = "ta :: 'a list set set set"
-apply (auto intro: ta_empty)
-done
-
-definition var :: "'a \<Rightarrow> 'a temporal_formula"
-where "var a = Abs_temporal_formula {L | L ls . ls \<in> L \<and> a \<in> set ls \<and> distinct ls}"
-
-lemma Rep_formula_var: "Rep_temporal_formula (var a) = 
-  { L | L ls . ls \<in> L \<and> a \<in> set ls \<and> distinct ls}"
-unfolding var_def using ta.var by (rule Abs_temporal_formula_inverse)
 
 notation
   bot ("\<bottom>") and
@@ -59,43 +10,132 @@ notation
   inf  (infixl "\<sqinter>" 70) and
   sup  (infixl "\<squnion>" 65)
 
-instantiation temporal_formula :: (type) boolean_algebra
+type_synonym 'a tval = "'a list set"
+
+inductive_set
+  ta :: "'a tval set"
+where
+  var: "{ a # ls | ls . a \<notin> set ls \<and> distinct ls } \<in> ta" |
+  Compl: "S \<in> ta \<Longrightarrow> - S \<in> ta" |
+  inter: "S \<in> ta \<Longrightarrow> T \<in> ta \<Longrightarrow> S \<inter> T \<in> ta"
+
+lemma ta_Diff: 
+  "S \<in> ta \<Longrightarrow> T \<in> ta \<Longrightarrow> S - T \<in> ta"
+unfolding Diff_eq by (intro ta.inter ta.Compl)
+
+lemma ta_union: "S \<in> ta \<Longrightarrow> T \<in> ta \<Longrightarrow> 
+  S \<union> T \<in> ta"
+proof -
+  assume "S \<in> ta" and "T \<in> ta"
+  hence "- (- S \<inter> - T) \<in> ta" by (intro ta.intros)
+  thus "S \<union> T \<in> ta" by simp
+qed
+
+lemma ta_empty: "({} :: 'a tval) \<in> ta"
+proof -
+  obtain S :: "'a tval" where "S \<in> ta" 
+    by (fast intro: ta.var)
+  hence "S \<inter> -S \<in> ta" by (intro ta.intros)
+  thus ?thesis by simp
+qed
+
+lemma ta_UNIV: "(UNIV :: 'a tval) \<in> ta"
+proof -
+  have "- {} \<in> ta" using ta_empty by (rule ta.Compl)
+  thus "UNIV \<in> ta" by simp
+qed
+
+typedef 'a tformula = "ta :: 'a tval set" by (auto intro: ta_empty)
+
+definition tvar :: "'a \<Rightarrow> 'a tformula"
+where "tvar a = Abs_tformula { a # ls | ls . a \<notin> set ls \<and> distinct ls }"
+
+lemma Rep_tformula_tvar : "Rep_tformula (tvar a) = { a # ls | ls . a \<notin> set ls \<and> distinct ls }"
+unfolding tvar_def using ta.var by (rule Abs_tformula_inverse)
+
+instantiation tformula :: (type) boolean_algebra
 begin
 
 definition
-  "x \<sqinter> y = Abs_temporal_formula (Rep_temporal_formula x \<inter> Rep_temporal_formula y)"
+  "x \<sqinter> y = Abs_tformula (Rep_tformula x \<inter> Rep_tformula y)"
+
+definition
+  "x \<squnion> y = Abs_tformula (Rep_tformula x \<union> Rep_tformula y)"
+
+definition
+  "\<top> = Abs_tformula UNIV"
+
+definition
+  "\<bottom> = Abs_tformula {}"
+
+definition
+  "x \<le> y \<longleftrightarrow> Rep_tformula x \<subseteq> Rep_tformula y"
+
+definition
+  "x < y \<longleftrightarrow> Rep_tformula x \<subset> Rep_tformula y"
+
+definition 
+  "- x = Abs_tformula (- Rep_tformula x)"
+
+definition
+  "x - y = Abs_tformula (Rep_tformula x - Rep_tformula y)"
+
+lemma Rep_tformula_inf:
+  "Rep_tformula (x \<sqinter> y) = Rep_tformula x \<inter> Rep_tformula y"
+unfolding inf_tformula_def
+by (intro Abs_tformula_inverse ta.inter Rep_tformula)
+
+lemma Rep_tformula_sup:
+  "Rep_tformula (x \<squnion> y) = Rep_tformula x \<union> Rep_tformula y"
+unfolding sup_tformula_def
+by (intro Abs_tformula_inverse ta_union Rep_tformula)
+
+lemma Rep_tformula_top: "Rep_tformula \<top> = UNIV"
+unfolding top_tformula_def by (intro Abs_tformula_inverse ta_UNIV)
+
+lemma Rep_tformula_bot: "Rep_tformula \<bottom> = {}"
+unfolding bot_tformula_def by (intro Abs_tformula_inverse ta_empty)
+
+lemma Rep_tformula_compl: "Rep_tformula (- x) = - Rep_tformula x"
+unfolding uminus_tformula_def
+by (intro Abs_tformula_inverse ta.Compl Rep_tformula)
+
+lemma Rep_tformula_diff:
+  "Rep_tformula (x - y) = Rep_tformula x - Rep_tformula y"
+unfolding minus_tformula_def
+by (intro Abs_tformula_inverse ta_Diff Rep_tformula)
+
+lemmas eq_tformula_iff = Rep_tformula_inject [symmetric]
+
+lemmas Rep_tformula_simps =
+  less_eq_tformula_def less_tformula_def eq_tformula_iff
+  Rep_tformula_sup Rep_tformula_inf Rep_tformula_top Rep_tformula_bot
+  Rep_tformula_compl Rep_tformula_diff Rep_tformula_tvar
 
 instance proof
-qed (unfold Rep_temporal_formula_simps, auto)
+qed (unfold Rep_tformula_simps, auto)
 
 end
 
-primrec set_temporal_exp :: "'a temporal_exp \<Rightarrow> 'a list set"
-where
-  "set_temporal_exp Top_temporal_exp = lists UNIV" |
-  "set_temporal_exp Bot_temporal_exp = { [] }" |
-  "set_temporal_exp (Atom_temporal_exp a) = unpile_temporal_exp [a] UNIV" |
-  "set_temporal_exp (Before_temporal_exp t1 t2) = 
-    { l1 @ l2 | l1 l2 . l1 \<in> set_temporal_exp t1 \<and> l2 \<in> set_temporal_exp t2  }" |
-  "set_temporal_exp (Or_temporal_exp t1 t2) = (set_temporal_exp t1) \<union> (set_temporal_exp t2)"
+lemma bot_neq_top_tformula [simp]: "(\<bottom> :: 'a tformula) \<noteq> \<top>"
+unfolding Rep_tformula_simps by auto
 
-datatype Test = Test_a | Test_b | Test_c
+lemma top_neq_bot_tformula [simp]: "(\<top> :: 'a tformula) \<noteq> \<bottom>"
+unfolding Rep_tformula_simps by auto
 
-lemma "set_temporal_exp (Before_temporal_exp (Atom_temporal_exp Test_a) (Atom_temporal_exp Test_b))
-  = {[Test_a, Test_b],[Test_a,Test_b,Test_c],[Test_a,Test_c,Test_b],[Test_c,Test_a,Test_b]}"
-apply (auto simp add: unpile_temporal_exp_def is_ordered_sublist_def)
+lemma var_le_tvar_simps [simp]:
+  "tvar i \<le> tvar j \<longleftrightarrow> i = j"
+  "\<not> tvar i \<le> - tvar j"
+  "\<not> - tvar i \<le> tvar j "
+unfolding Rep_tformula_simps 
+apply (auto)
 done
 
-
-definition val_temporal_exp :: "'a temporal_exp \<Rightarrow> 'a list set \<Rightarrow> bool"
-where
-  "val_temporal_exp t s = (set_temporal_exp t \<subseteq> s)"
-
-lemma "val_temporal_exp (Before_temporal_exp (Atom_temporal_exp A) (Atom_temporal_exp B))"
-
-type_synonym ('a, 'FMode, 'vv) TemporalConditionalMode =
-  "('a list set, 'FMode, 'vv) ConditionalMode"
-
-lemma temporal_and: "CVIF {[A,B], [B,A]} t1 t2"
+lemma var_eq_tvar_simps [simp]:
+  "tvar i = tvar j \<longleftrightarrow> i = j"
+  "tvar i \<noteq> - tvar j"
+  "- tvar i \<noteq> tvar j"
+apply (metis var_le_tvar_simps(1))
+done
 
 end
